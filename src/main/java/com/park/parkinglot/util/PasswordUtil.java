2a2e0bcc675ac90dc4a15c830a2d7a2cd4dabba3
SonfaleanBogdanIoan <72549134+iStillStealSteel@users.noreply.github.com>
2022-01-02 17:24:05 +0200

    a

--- src/main/java/com/park/parkinglot/util/PasswordUtil.java
+++ src/main/java/com/park/parkinglot/util/PasswordUtil.java
@@ -4,28 +4,27 @@
  */
 package com.park.parkinglot.util;
 
-import com.sun.istack.internal.logging.Logger;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author boo_b
  */
 public class PasswordUtil {
-    
-    public static String convertToSha256(String password){
-        try{
+
+    public static String convertToSha256(String password) {
+        try {
             MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
             messageDigest.update(password.getBytes());
-            byte[] digest=messageDigest.digest();
-            String result = new BigInteger(1,digest).toString(16);
+            byte[] digest = messageDigest.digest();
+            String result = new BigInteger(1, digest).toString(16);
             return result;
-        }
-        catch(NoSuchAlgorithmException ex){
-            Logger.getLogger(PasswordUtil.class.getName()).log(Level.SEVERE,null,ex);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(PasswordUtil.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
